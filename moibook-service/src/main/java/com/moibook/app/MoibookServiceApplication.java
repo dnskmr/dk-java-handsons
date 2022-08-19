168215d213416604b2030660961aa74cff499583
dnskmr <dinesh220692@gmail.com>
2022-08-19 19:30:05 +0530

    [DK] - [MS] - Moibook Service Initial Commit

---
+++ moibook-service/src/main/java/com/moibook/app/MoibookServiceApplication.java
@@ -0,0 +1,22 @@
+package com.moibook.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dinesh
+ * @version 1.0
+ * @since 08/19/2022
+ *
+ * <p>
+ * The MoiBookService Application provides list of services for the Moi Book Application
+ * </p>
+ */
+@SpringBootApplication
+public class MoibookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoibookServiceApplication.class, args);
+    }
+
+}
