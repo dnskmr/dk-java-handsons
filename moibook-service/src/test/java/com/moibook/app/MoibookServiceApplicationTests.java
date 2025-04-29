34becdd447829516aeeaf843ff08dc5dd223e501
dnskmr <dinesh220692@gmail.com>
2025-04-30 01:40:18 +0530

    [DK] - [MS] - Moibook Service Initial Commit

---
+++ moibook-service/src/test/java/com/moibook/app/MoibookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moibook.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoibookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
