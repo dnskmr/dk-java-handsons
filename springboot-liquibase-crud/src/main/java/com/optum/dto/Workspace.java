61b6e73568d009b9eebe3158b861b0b737804b6e
dnskmr <dinesh220692@gmail.com>
2025-05-08 23:31:34 +0530

    [Spring Security Project Implementation]
    [Handson] - [Spring Security Project Implementations]

--- springboot-liquibase-crud/src/main/java/com/optum/dto/Workspace.java
+++ springboot-liquibase-crud/src/main/java/com/optum/dto/Workspace.java
@@ -1,5 +1,6 @@
 package com.optum.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -11,6 +12,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Workspace {
 
     @Id
